da2a80bfce02971e7040fdb8a94dcc051b4e8543
whenaska <uak0709@mail.ru>
2022-11-29 15:11:02 +0400

    popup + changes in form + little changes in mobile version

--- maket/src/js/script.js
+++ maket/src/js/script.js
@@ -5,3 +5,22 @@ let swiper = new Swiper(".mySwiper", {
     },
     loop: true,
 });
+
+let Popup_request = document.getElementById("popup-request");
+let Dark_bg = document.getElementById("dark-bg");
+
+function F_check_popup_request() {
+    if((document.getElementById("btn-open-popup-request-1").click)||
+    (document.getElementById("btn-open-popup-request-2").click)||
+    (document.getElementById("btn-open-popup-request-3").click)||
+    (document.getElementById("btn-open-popup-request-4").click)){
+
+        Popup_request.classList.add("Open");
+        Dark_bg.classList.add("Active");
+    }
+}
+
+function closePopup_request() {
+    Popup_request.classList.remove("Open");
+    Dark_bg.classList.remove("Active");
+}
