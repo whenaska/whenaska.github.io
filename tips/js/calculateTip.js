374f47043605d21ebe2e632e186cbb7015d0ef9a
whenaska <uak0709@mail.ru>
2023-03-28 17:48:22 +0400

    fix tips

--- tips/js/calculateTip.js
+++ tips/js/calculateTip.js
@@ -1,11 +1,11 @@
 let moneyFromEach;
 
 function calculateTip(){
-    let total = document.getElementById("bill").value;
-    let numGuests = document.getElementById("guests").value;
-    let percTip = document.getElementById("percentTips").value;
+    let total = +document.getElementById("bill").value;
+    let numGuests = +document.getElementById("guests").value;
+    let percTip = +document.getElementById("percentTips").value;
 
-    moneyFromEach = (total / numGuests * percTip / 100);
+    moneyFromEach = (total + total * percTip / 100) / numGuests;
     moneyFromEach = +moneyFromEach.toFixed(2);
 
     changePopup();
