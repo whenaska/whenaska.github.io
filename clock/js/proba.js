91dfe9db30e9d13abab195c035584826d9f85406
whenaska <uak0709@mail.ru>
2023-05-06 01:44:00 +0400

    png to jpg + simplifying js code (i hope so)

--- clock/js/proba.js
+++ clock/js/proba.js
@@ -1,40 +1,25 @@
 let seconds = -100;
+let allImg = document.querySelectorAll("img");
+let arrSrcNumbers = ["img/0.jpg", "img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg", "img/5.jpg", "img/6.jpg", "img/7.jpg", "img/8.jpg", "img/9.jpg"];
 
-let hour1Img = document.getElementById("hour1");
-let hour2Img = document.getElementById("hour2");
-
-let min1Img = document.getElementById("min1");
-let min2Img = document.getElementById("min2");
-
-let sec1Img = document.getElementById("sec1");
-let sec2Img = document.getElementById("sec2");
-
-
-let arrSrcNumbers = ["img/0.png", "img/1.png", "img/2.jpg", "img/3.png", "img/4.png", "img/5.png", "img/6.png", "img/7.png", "img/8.png", "img/9.png"];
-
-function doStuff() {
+function changeTime() {
     let time = new Date();
+    if (seconds == time.getSeconds()) return;
 
-    let hour1 = Math.floor(time.getHours() / 10);
-    let hour2 = time.getHours() % 10;
+    let arrTime = [`${time.getHours()}`, `${time.getMinutes()}`, `${time.getSeconds()}`];
+    let cntImg = 0;
+    let timeNow;
 
-    let min1 = Math.floor(time.getMinutes() / 10);
-    let min2 = time.getMinutes() % 10;
+    for(timeArr of arrTime){
+        timeArr.length == 2 ? timeNow = timeArr : timeNow = "0" + timeArr;
+       
+        allImg[cntImg].src = arrSrcNumbers[timeNow[0]];
+        allImg[cntImg+1].src = arrSrcNumbers[timeNow[1]];
 
-    let sec1 = Math.floor(time.getSeconds() / 10);
-    let sec2 = time.getSeconds() % 10;
-
-    if (seconds != time.getSeconds()) {
-        hour1Img.src = arrSrcNumbers[hour1];
-        hour2Img.src = arrSrcNumbers[hour2];
-
-        min1Img.src = arrSrcNumbers[min1];
-        min2Img.src = arrSrcNumbers[min2];
-
-        sec1Img.src = arrSrcNumbers[sec1];
-        sec2Img.src = arrSrcNumbers[sec2];
-    }
+        cntImg += 4;
+    };
 
     seconds = time.getSeconds();
 }
-setInterval(doStuff, 1);
+
+setInterval(changeTime, 100);
